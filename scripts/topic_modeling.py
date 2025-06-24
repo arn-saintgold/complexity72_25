82c87748bed213280aee47e794b20b1f2d0a87a2
arn-saintgold <arnaldo.santoro@unive.it>
2025-06-24 22:51:56 +0200

    topic model script almost ready

--- scripts/topic_modeling.py
+++ scripts/topic_modeling.py
@@ -59,8 +59,6 @@ def topic_modeling(filename, text_column, *args, **kwargs):
     embeddings = np.load(filename+'.npy', allow_pickle=True).item() if os.path.exists(filename+'.npy') else None
     if embeddings is None: 
         embeddings = embedding_model.encode(texts)
-    print('SUCCESS')
-    exit()
 
     # Create a CountVectorizer
     vectorizer = CountVectorizer(stop_words='english')
@@ -69,17 +67,13 @@ def topic_modeling(filename, text_column, *args, **kwargs):
     best_params = search_params(embeddings)
     umap_model = UMAP(n_neighbors=best_params[0], n_components=best_params[1], min_dist=0.0, metric='cosine')
     hdbscan_model = HDBSCAN(min_cluster_size=best_params[2], metric='euclidean', cluster_selection_method=best_params[3], prediction_data=True, gen_min_span_tree=True) 
-    # Create a UMAP model for dimensionality reduction
-    umap_model = UMAP(n_neighbors=15, n_components=5, metric='cosine')
-    
-    # Create an HDBSCAN model for clustering
-    hdbscan_model = HDBSCAN(min_cluster_size=10, metric='euclidean', cluster_selection_method='eom')
-    
+    #HDBSCAN(min_cluster_size=min_cluster_size, metric='euclidean', cluster_selection_method=cluster_selection_method, prediction_data=True, gen_min_span_tree=True)
+     
     # Create a BERTopic model
     topic_model = BERTopic(
         embedding_model=embedding_model,
         umap_model=umap_model,
-        hdbscan_model = hdbscan_model,
+        hdbscan_model =hdbscan_model,
         vectorizer_model=CountVectorizer(ngram_range=(1, 2), stop_words='english'),
         ctfidf_model=ClassTfidfTransformer(),
         #representation_model=
@@ -89,17 +83,19 @@ def topic_modeling(filename, text_column, *args, **kwargs):
         language='english',
     )
 
-    topic_model = BERTopic(vectorizer_model=ClassTfidfTransformer(), umap_model=umap_model, hdbscan_model=hdbscan_model,
-                           calculate_probabilities=True, verbose=True)
+    #topic_model = BERTopic(vectorizer_model=ClassTfidfTransformer(), umap_model=umap_model, hdbscan_model=hdbscan_model,
+    #                       calculate_probabilities=True, verbose=True)
+    
     # Fit the model to the texts
     topics, _ = topic_model.fit_transform(texts, embeddings=embeddings)
     
     # Save the model
-    model_filename = os.path.splitext(filename)[0] + '_topic_model'
-    topic_model.save(model_filename)    
+    model_filename = filename.split('/')[-1] + '.topic_model'
+    topic_model.save(os.path.join('models',model_filename.split()[-1]))
     print(f"Topic model saved to {model_filename}.")
 
-
+    # TODO return dataset with topics
+    # TODO do also dynamic topic modeling
     return topics, topic_model.get_topic_info()
 
 def count_months_passed(df, col_name):
@@ -128,7 +124,7 @@ def main():
     topics, topic_info = topic_modeling(args.filename, args.text_column, args.optional_columns)
     print(f"Identified {len(set(topics))} topics.")
     print("Topics identified:")
-    print(topic_info)
+    #print(topic_info)
 
 if __name__ == '__main__':
     main()
