11f557cbbf362bde9c5ecb8071fee3a1b51313dc
arn-saintgold <arnaldo.santoro@unive.it>
2025-06-25 10:35:03 +0200

    bert model ready

--- scripts/topic_modeling.py
+++ scripts/topic_modeling.py
@@ -20,111 +20,158 @@ def search_params(embeddings):
     best_params = None
     for n_neighbors in [15, 50]:
         for n_components in [2, 5, 10, 20, 50]:
-            umap_model = UMAP(n_neighbors=n_neighbors, n_components=n_components, min_dist=0.0, metric='cosine')
+            umap_model = UMAP(
+                n_neighbors=n_neighbors,
+                n_components=n_components,
+                min_dist=0.0,
+                metric="cosine",
+            )
             reduced_embeddings = umap_model.fit_transform(embeddings)
             for min_cluster_size in [10, 20, 50, 100, 200, 500]:
-                for cluster_selection_method in ['eom', 'leaf']:
-                    hdbscan_model = HDBSCAN(min_cluster_size=min_cluster_size, metric='euclidean', cluster_selection_method=cluster_selection_method, prediction_data=True, gen_min_span_tree=True)
-                    relative_validity = hdbscan_model.fit(reduced_embeddings).relative_validity_
+                for cluster_selection_method in ["eom", "leaf"]:
+                    hdbscan_model = HDBSCAN(
+                        min_cluster_size=min_cluster_size,
+                        metric="euclidean",
+                        cluster_selection_method=cluster_selection_method,
+                        prediction_data=True,
+                        gen_min_span_tree=True,
+                    )
+                    relative_validity = hdbscan_model.fit(
+                        reduced_embeddings
+                    ).relative_validity_
                     if relative_validity > max_relative_validity:
                         max_relative_validity = relative_validity
-                        best_params = (n_neighbors, n_components, min_cluster_size, cluster_selection_method,)
-    print(f"Best parameters: n_neighbors={best_params[0]}, n_components={best_params[1]}, min_cluster_size={best_params[2]}, cluster_selection_method={best_params[3]}, max_relative_validity={max_relative_validity}")
-    return(best_params)
+                        best_params = (
+                            n_neighbors,
+                            n_components,
+                            min_cluster_size,
+                            cluster_selection_method,
+                        )
+    print(
+        f"Best parameters: n_neighbors={best_params[0]}, n_components={best_params[1]}, min_cluster_size={best_params[2]}, cluster_selection_method={best_params[3]}, max_relative_validity={max_relative_validity}"
+    )
+    return best_params
+
 
 def clean_dataframe(df, col_name):
-    return df[df[col_name].str.len()>0]
+    return df[df[col_name].str.len() > 0]
 
-def topic_modeling(filename, text_column, *args, **kwargs):
+
+def topic_modeling(
+    filename, text_column, embedding_model_name="all-MiniLM-L6-v2", *args, **kwargs
+):
     # Load the data
-    if filename.endswith('.parquet'):
+    if filename.endswith(".parquet"):
         df = clean_dataframe(pd.read_parquet(filename), text_column)
-    elif filename.endswith('csv'):
-        df = clean_dataframe(pd.read_csv(filename),text_column)
+    elif filename.endswith("csv"):
+        df = clean_dataframe(pd.read_csv(filename), text_column)
     else:
         raise ValueError("Extension not recognized")
     # Take subset of data
 
     # embedding_model
-    embedding_model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
+    embedding_model = SentenceTransformer(embedding_model_name, device="cuda")
 
     # Check if the specified text column exists
     if text_column not in df.columns:
         raise ValueError(f"Column '{text_column}' does not exist in the DataFrame.")
-    
-    
+
     # Extract the text data
     texts = df[text_column].astype(str).tolist()
-    
-    embeddings = np.load(filename+'.npy', allow_pickle=True).item() if os.path.exists(filename+'.npy') else None
-    if embeddings is None: 
+
+    embeddings = (
+        np.load(filename + ".npy", allow_pickle=True).item()
+        if os.path.exists(filename + ".npy")
+        else None
+    )
+    if embeddings is None:
         embeddings = embedding_model.encode(texts)
 
     # Create a CountVectorizer
-    vectorizer = CountVectorizer(stop_words='english')
-    
+    vectorizer = CountVectorizer(stop_words="english")
+
     # Search for the best parameters for UMAP and HDBSCAN
     best_params = search_params(embeddings)
-    umap_model = UMAP(n_neighbors=best_params[0], n_components=best_params[1], min_dist=0.0, metric='cosine')
-    hdbscan_model = HDBSCAN(min_cluster_size=best_params[2], metric='euclidean', cluster_selection_method=best_params[3], prediction_data=True, gen_min_span_tree=True) 
-    #HDBSCAN(min_cluster_size=min_cluster_size, metric='euclidean', cluster_selection_method=cluster_selection_method, prediction_data=True, gen_min_span_tree=True)
-     
+    umap_model = UMAP(
+        n_neighbors=best_params[0],
+        n_components=best_params[1],
+        min_dist=0.0,
+        metric="cosine",
+    )
+    hdbscan_model = HDBSCAN(
+        min_cluster_size=best_params[2],
+        metric="euclidean",
+        cluster_selection_method=best_params[3],
+        prediction_data=True,
+        gen_min_span_tree=True,
+    )
+    # HDBSCAN(min_cluster_size=min_cluster_size, metric='euclidean', cluster_selection_method=cluster_selection_method, prediction_data=True, gen_min_span_tree=True)
+
     # Create a BERTopic model
     topic_model = BERTopic(
         embedding_model=embedding_model,
         umap_model=umap_model,
-        hdbscan_model =hdbscan_model,
-        vectorizer_model=CountVectorizer(ngram_range=(1, 2), stop_words='english'),
+        hdbscan_model=hdbscan_model,
+        vectorizer_model=CountVectorizer(ngram_range=(1, 2), stop_words="english"),
         ctfidf_model=ClassTfidfTransformer(),
-        #representation_model=
-
+        # representation_model=
         verbose=True,
         calculate_probabilities=True,
-        language='english',
+        language="english",
     )
 
-    #topic_model = BERTopic(vectorizer_model=ClassTfidfTransformer(), umap_model=umap_model, hdbscan_model=hdbscan_model,
+    # topic_model = BERTopic(vectorizer_model=ClassTfidfTransformer(), umap_model=umap_model, hdbscan_model=hdbscan_model,
     #                       calculate_probabilities=True, verbose=True)
-    
+
     # Fit the model to the texts
     topics, _ = topic_model.fit_transform(texts, embeddings=embeddings)
-    
+
     # Save the model
-    model_filename = filename.split('/')[-1] + '.topic_model'
-    topic_model.save(os.path.join('models',model_filename.split()[-1]))
+    model_filename = filename.split("/")[-1] + ".topic_model"
+    topic_model.save(os.path.join("models", model_filename.split()[-1]))
     print(f"Topic model saved to {model_filename}.")
 
     # TODO return dataset with topics
-    # TODO do also dynamic topic modeling
-    return topics, topic_model.get_topic_info()
+    return topic_model.get_topic_info(), topic_model.get_document_info(texts)
+
 
 def count_months_passed(df, col_name):
-
-
     df[col_name] = pd.to_datetime(df[col_name])
 
     min_date = df[col_name].min()
     max_date = df[col_name].max()
 
     # Calculate months difference
-    months_passed = (max_date.year - min_date.year) * 12 + (max_date.month - min_date.month)
-    return(months_passed)
+    months_passed = (max_date.year - min_date.year) * 12 + (
+        max_date.month - min_date.month
+    )
+    return months_passed
+
 
 def main():
     parser = argparse.ArgumentParser(description="Perform topic modeling on text data.")
-    parser.add_argument("filename", type=str, help="Path to the CSV file containing the text data.")
-    parser.add_argument("text_column", type=str, help="Name of the column containing the text data.")
-    parser.add_argument("--optional_columns", nargs='*', help="Optional columns for temporal or categorical analysis.")
+    parser.add_argument(
+        "filename", type=str, help="Path to the CSV file containing the text data."
+    )
+    parser.add_argument(
+        "text_column", type=str, help="Name of the column containing the text data."
+    )
+    parser.add_argument(
+        "--optional_columns",
+        nargs="*",
+        help="Optional columns for temporal or categorical analysis.",
+    )
 
-    
     args = parser.parse_args()
 
     print(f"Reading file: {args.filename}\nText column: {args.text_column}")
 
-    topics, topic_info = topic_modeling(args.filename, args.text_column, args.optional_columns)
-    print(f"Identified {len(set(topics))} topics.")
-    print("Topics identified:")
-    #print(topic_info)
+    topic_info, document_info = topic_modeling(
+        args.filename, args.text_column, args.optional_columns
+    )
+    print(f"Identified {len(set(topic_info)-1)} topics.")
+    print(f"Noise percentage: {round(100*(len(document_info.query("Topic == -1"))/len(document_info)),2)}%")
+    # print(topic_info)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
